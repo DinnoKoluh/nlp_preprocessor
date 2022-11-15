7b344068cc307feaffdb2ce2cea032869d7b4c3f
DinnoKoluh <koluh.dinno@gmail.com>
2022-11-15 17:42:40 +0300

    First Commit. More will come :)

---
+++ NLP.py
@@ -0,0 +1,249 @@
+class NLP:
+    rough_tokens = []
+    tokens = []
+    sentences = []
+    text = []
+    vocabulary = []
+    word_frequency = {}
+    stems = []
+    puncs = ['.', ',', '!', '?', '(', ')', ':', ';', '"', '"', '\'']
+    email_puncs = [',', '!', '?', '(', ')', ':', ';', '"', '"', '\'']
+    web_puncs = [',', '!', '?', '(', ')', ';', '"', '"']
+    lex = ['m.p.h.', 'mr.', 'Mr.', 'Mrs.', 'i.e.', 'fig.', 'Fig.', 'al.']
+
+    def __init__(self, text):
+        self.text = text
+
+    def tokenize_rough(self):
+        splitters = [' ', '\n'] # chars used for splitting words
+        #splitters = ['.', '!', '?']
+        word = '' # where to store a single word
+        i = 0
+        while i < len(self.text):
+            split = False # flag if we are going to split e.g. add a new token
+            while self.text[i] in splitters: # while the i-th character is among splitters keep splitting
+                split = True
+                i = i + 1 # skip that position in text
+                if i == len(self.text):
+                    break
+
+            if split:
+                self.rough_tokens.append(word)
+                word = '' # reset for next word
+                continue
+            word = word + self.text[i] # adding letters to words
+            i = i + 1
+        self.rough_tokens.append(word) # appending the last word in a text
+    
+    def tokenize(self):
+        self.tokenize_rough() # do the rough tokenization first
+        self.tokens = self.rough_tokens
+        i = 0
+        while i < len(self.tokens):
+            word = self.tokens[i] 
+            # check if word is in lexicon
+            if self.is_in_lex(word):
+                i = i + 1
+                continue    
+            
+            #websites
+            if self.has_web_address(word):
+               #puncs.remove('.')
+               #puncs.remove(':')
+                i = i + 1
+            
+            sub_tokens = self.split_token(word, self.puncs) # where to store subtokens found in a word
+            
+            #inserting sub_tokens into tokens
+            if len(sub_tokens) > 0:
+                del self.tokens[i] # delete the current token where the sub tokens will be substituted
+                for item in sub_tokens:
+                    self.tokens.insert(i, item)
+                    i = i + 1
+            else:
+                i = i + 1
+
+    def sentence_split(self):
+        if len(self.rough_tokens) == 0:
+            print("Tokenize the text first!")
+            return
+        # using the rough_tokens rebuilding the text again and splitting it into sentences based on rules
+        sentence = ''
+        i = 0
+        while i < len(self.rough_tokens):
+            if self.has_punctuation(self.rough_tokens[i]):
+                if (self.is_in_lex(self.rough_tokens[i]) or self.has_email(self.rough_tokens[i]) or
+                    self.has_abbreviation(self.rough_tokens[i]) or self.has_number(self.rough_tokens[i])): # if token is in lexicon or is an email address
+                    sentence = sentence + self.rough_tokens[i] + ' '
+                    i = i + 1
+                    continue
+                sentence = sentence + self.rough_tokens[i] # adding the last token in sentence
+                self.sentences.append(sentence) # adding sentence to sentences
+                sentence = '' # restarting sentence 
+            else:
+                sentence = sentence + self.rough_tokens[i] + ' ' # concatenating tokens with whitespace
+            i = i + 1
+
+    def make_vocabulary(self, sorted_voc=False):
+        if len(self.tokens) == 0:
+            print("Tokenize text first!")
+            return 
+        for token in self.tokens:
+            if not(token in self.word_frequency.keys()): 
+                self.word_frequency[token] = self.tokens.count(token)
+        # sorting word frequency
+        self.word_frequency = dict(sorted(self.word_frequency.items(), key=lambda x: x[1], reverse=True))
+        # make vocabulary
+        self.vocabulary = list(self.word_frequency.keys())
+        if sorted_voc:
+            self.vocabulary.sort()
+
+    def split_token(self, token, puncs):
+        sub_tokens = [] # where to store subtokens found in a word
+        j = 0
+        if self.has_number(token): return sub_tokens # if token is number with ., return empty 
+        if self.has_clitic(token): return self.split_by_char(token, '\'', self.puncs) # if token has a clitic split it 
+        if self.has_abbreviation(token): return sub_tokens
+        if self.has_email(token): return self.split_by_char(token, '@', self.email_puncs)
+        #if self.has_web_address(token): return sub_tokens
+
+        while j < len(token):
+            if token[j] in puncs:
+                if j > 0: sub_tokens.append(token[0:j]) # insert left over word only if its length is bigger than 0
+                sub_tokens.append(token[j]) # insert punc into sub tokens
+                token = token[j+1:] # the new word is the leftover
+                j = -1 # reset counter to -1 (it will be zero in the next line)
+            j = j + 1
+        if len(token) > 0: sub_tokens.append(token) # add the last part of the word to sub_tokens if it is not empty
+        return sub_tokens
+
+    def split_by_char(self, token, character, puncs):
+        sub_tokens = []
+        i = 0
+        # Find the position of the first clitic
+        while i < len(token):
+            if i > 0 and i < (len(token) - 1) and token[i] == character:
+                if self.is_letter(token[i-1]) and self.is_letter(token[i+1]):
+                    break
+            i = i + 1
+        # done recursively for an arbitrary number of clitics in token
+        sub_tokens1 = self.split_token(token[0:i], puncs) # take the string up to the first clitic and split it
+        sub_tokens2 = self.split_token(token[i+1:], puncs) # take the string after the first clitic and split it
+        sub_tokens = sub_tokens + sub_tokens1[0:-1] # subtokens before the first clitic
+        # the last element of subtokens before the first clitic and first element of subtokens after the first clitic form that clitic
+        sub_tokens.append(sub_tokens1[-1] + character + sub_tokens2[0]) 
+        sub_tokens = sub_tokens + sub_tokens2[1:] # subtokens after the first clitic
+        return sub_tokens
+    
+    def split_by_string(self, token, str, puncs):
+        print("dinno")
+
+# HELPER FUNCTIONS
+    # if a character is a letter
+    def is_letter(self, c):
+        return self.is_lower_case(c) or self.is_upper_case(c)
+    
+    # if a character is lower case
+    def is_lower_case(self, c):
+        return c >= 'a' and c <= 'z'
+
+    # if a character is upper case
+    def is_upper_case(self, c):
+        return c >= 'A' and c <= 'Z'
+
+    # if a token has a substring that is in the lexicon
+    def is_in_lex(self, token):
+        for l in self.lex:
+            if l in token:
+                return True
+        return False
+
+    # if a token has a punctuation 
+    def has_punctuation(self, token):
+        puncs = ['.', '!', '?']
+        for pun in puncs:
+            if pun in token:
+                return True
+        return False
+
+    # if a character is a number
+    def is_number(self, c):
+        return c >= '0' and c <= '9'
+
+    #if a token has a legit number inside like 4,3 or 4.56M
+    def has_number(self, token):
+        i = 0
+        while i < len(token):
+            if token[i] == '.' or token[i] == ',':
+                if i > 0 and i < (len(token) - 1) and self.is_number(token[i-1]) and self.is_number(token[i+1]):
+                    return True
+            i = i + 1
+        return False
+
+    #if the end of the token is an abbreviation for a name i.e. in the form "D.K."
+    def has_abbreviation(self, token): 
+        return len(token) > 1 and token[-1] == '.' and self.is_upper_case(token[-2])
+    
+    #if token has @ (i.e. has email address) 
+    def has_email (self, token):
+        return '@' in token
+
+    # if token has domain name
+    def has_web_address (self, token):
+        return 'http' in token or 'www' in token
+
+    # if token has clitic
+    def has_clitic (self, token):
+        i = 0
+        while i < len(token):
+            if i > 0 and i < (len(token) - 1) and token[i] == '\'':
+                if self.is_letter(token[i-1]) and self.is_letter(token[i+1]):
+                    return True
+            i = i + 1
+        return False
+
+
+'''
+def split_clitic(self, token, puncs):
+        sub_tokens = []
+        i = 0
+        # Find the position of the first clitic
+        while i < len(token):
+            if i > 0 and i < (len(token) - 1) and token[i] == '\'':
+                if self.is_letter(token[i-1]) and self.is_letter(token[i+1]):
+                    break
+            i = i + 1
+        # done recursively for an arbitrary number of clitics in token
+        sub_tokens1 = self.split_token(token[0:i], puncs) # take the string up to the first clitic and split it
+        sub_tokens2 = self.split_token(token[i+1:], puncs) # take the string after the first clitic and split it
+        sub_tokens = sub_tokens + sub_tokens1[0:-1] # subtokens before the first clitic
+        # the last element of subtokens before the first clitic and first element of subtokens after the first clitic form that clitic
+        sub_tokens.append(sub_tokens1[-1] + '\'' + sub_tokens2[0]) 
+        sub_tokens = sub_tokens + sub_tokens2[1:] # subtokens after the first clitic
+        return sub_tokens
+
+def sentence_split1(text):
+    splitters = ['.', '!', '?']
+    sentences = [] # where to store all the words
+    word = '' # where to store a single word
+    i = 0
+    text = text.translate({ord('\n'): None})
+    while i < len(text):
+        j = 0
+        split = False # flag if we are going to split e.g. add a new token
+        while text[i] in splitters: # while the i-th character is among splitters keep splitting
+            if is_upper_case(text[i-1]): break
+            if i < len(text)-1 and text[i+1] != ' ': break
+            split = True
+            i = i + 1 # skip that position in text
+            if i == len(text):
+                break
+
+        if split:
+            sentences.append(word)
+            word = '' # reset for next word
+            continue
+        word = word + text[i] # adding letters to words
+        i = i + 1
+    return text, sentences
+'''
---
+++ funs.py
@@ -0,0 +1,5 @@
+def get_text(txt):
+    f = open('data\\'+ str(txt)+'.txt', 'r')
+    return f.read()
+          
+
