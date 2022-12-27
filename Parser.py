ec2f7aac0d5ab03c53d5dd7eb9374f8a694b9ab4
DinnoKoluh <koluh.dinno@gmail.com>
2022-12-27 21:00:26 +0300

    Done further work on CKY parser. Added parse table outputing.

--- Parser.py
+++ Parser.py
@@ -3,10 +3,6 @@ import nltk
 from grammar import *
 
 class Parser:
-    text = ""
-    parsed_text = ""
-    tokens = ""
-
     def __init__(self, text):
         self.reset()
         self.text = text
@@ -14,21 +10,14 @@ class Parser:
         tokenizer.tokenize()
         self.tokens = tokenizer.dirty_tokens
         self.parsed_text = nltk.pos_tag(self.tokens)
+        self.table = [[[] for i in range(len(self.tokens))] for j in range(len(self.tokens))]
+        self.my_table = [[[] for i in range(len(self.tokens))] for j in range(len(self.tokens))]
     
     def reset(self):
         self.text = []
         self.parsed_text = []
         self.tokens = []
-
-    def make_CKY_table(self, length):
-        """
-        Function to make a template of the CKY table based on the length of the 
-        sequence of words.
-        """
-        table = [None] * length
-        for i in range (length):
-            table[i] = [None] * (length-i)
-        return table
+        self.my_table = []
 
     def get_all_tags(self, index):
         """
@@ -36,19 +25,17 @@ class Parser:
         The input is the index of the token in the sentence.
         """
         tags = []
-        pos = self.parsed_text[index][1] # getting the PoS of the token
-        if index == 0:
-            tags.append("S")
+        tags.append(self.parsed_text[index][1]) # getting the PoS of the token
         flag = True
         while(flag):
             flag = False
             for key in rules.keys():
                 for rule in rules[key]:
                     # if the PoS is the singular rule then add the key of the dictionary that corresponds to that PoS
-                    if pos == rule[0] and len(rule) == 1 and not(key in tags):
+                    if rule[0] in tags and len(rule) == 1 and not(key in tags):
                         tags.append(key)
-                        pos = key
                         flag = True
+        #del tags[0] # deleting the Penn treebank tag at the first position
         return tags
     
     def check_possible_pairs(self, set1, set2): 
@@ -57,40 +44,66 @@ class Parser:
         two tags from two sets of tags.
         """
         tag_rules = []
+        tags = []
         product = [[a, b] for a in set1 for b in set2] # cartesian product of two sets
         for p in product:
             for key in rules.keys():
                 for rule in rules[key]:
-                    if rule == p: # if the rule is among the product add it to the tags
-                        tag_rules.append([key, p])
-                        break
-        return tag_rules
+                    if rule == p and not(key in tags): # if the rule is among the product add it to the tags
+                        tag_rules.append(p)
+                        tags.append(key)
+                        #break
+        # use as of now, later will be abolished when multiple rules can be output
+        return tags, tag_rules
 
     def parse(self):
-        table = self.make_CKY_table(len(self.tokens))
-        links = [None] * (len(table)-1)
-        for j in range (len(table)):
-            table[j][0] = self.get_all_tags(j)
-            # print("column " + str(j))
+        links = []
+
+        for j in range (len(self.table)):
+            self.table[j][j] = self.get_all_tags(j)
             for i in range(j-1, -1, -1):
-                # j is shifted as (j - i)
-                # print("Current table entry: [" + str(i) + " " + str(j-i) + "]")
-                # print("Set1: " + str(table[i][0]))
-                # print("Set2: " + str(table[i+1][j-i-1]))
-                tag_rules = self.check_possible_pairs(table[i][0], table[i+1][j-i-1])
-                tag = []
-                if len(tag_rules) > 0:
-                    tag = [tag_rules[0][0]]
-                table[i][j-i] = tag
-
-                if j == len(table) - 1:
-                    links[i] = tag_rules[0]
+                for k in range(i,j):
+                    tags, tag_rules = self.check_possible_pairs(self.table[i][k], self.table[k+1][j]) # left cell, lower cell
+                    self.table[i][j].extend(tags)
+                    for tag, tag_rule in zip(tags, tag_rules):
+                        self.my_table[i][j].append([(i,k, tag_rule[0]), (k+1,j, tag_rule[1]), tag])
+                    # for tag_rule in tag_rules:
+                    #     tag_rule.append([(i,j), (i, k), (k+1, j)])
+                    links.extend(tag_rules)
+        return self.table, links, self.my_table
+    
+    def make_tree(self):
+        n_parses = self.table[0][-1].count("S")
+        trees = []
+        tree_index = 0
+        for cell in self.my_table[0][-1]:
+            if cell[2] == "S":
+                trees.append(["S"])
+                left_cell = cell[0][0:2]
+                bottom_cell = cell[1][0:2]
                 
-        return table, links
+                #nth_list = [my_table]
+                # left cell empty
+                if self.my_table[left_cell[0]][left_cell[1]] == []:
+                    pass
+                    #nth_list.append(c[])
 
-        # I eat sushi
-        # [S      [NP    [I]]    [VP      [Verb    [eat]]      [Nominal     [Sushi] ]]]
-            
+                tree_index = tree_index + 1
+    
+    def gen_tree(self, node):
+        if node[0][0] == node[0][1]:
+            index = node[0][0]
+            return f"[{self.my_table[index][index]} '{self.tokens[index]}']"
+        node1_index = node[0][0:2]
+        node2_index = node[1][0:2]
+        #return f"[{node[3]} {self.gen_tree(self.my_table[][])} {}]"
 
-
-        
+    def save_table(self, path):
+        """
+        Function to save the parse table as a .txt file.
+        """
+        f = open(path,'w')
+        for tab in self.table:
+            for t in tab:
+                print("{0:25}".format(str(t)), file = f, end = "")
+            print("", file=f) 
--- grammar.py
+++ grammar.py
@@ -1,14 +1,14 @@
 # Penn Treebank tagset: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 rules = {
-    "S": [["NP", "VP"], ["Aux", "NP", "VP"]],
-    "NP": [["Det", "Nominal"], ["Nominal"], ["Pronoun"], ["Pronoun", "Nominal"], ["Det", "Adjective"], ["Adjective", "Noun"]],
+    "S": [["NP", "VP"], ["Aux", "NP", "VP"], ["Verb", "NP"], ["VP", "PP"]],
+    "NP": [["Det", "Nominal"], ["Pronoun"], ["Pronoun", "Nominal"], ["Det", "Adjective"], ["Adjective", "Noun"], ["NNP"]],
     "VP": [["Verb"], ["Verb", "NP"], ["Verb", "Noun"], ["VP", "PP"], ["Verb", "NP", "PP"], ["Verb", "PP"]],
-    "PP": [["Preposition", "NP"]],
+    "PP": [["Prep", "NP"], ["Prep", "Nominal"]],
     "Nominal": [["Nominal", "Noun"], ["Nominal", "PP"], ["Noun"], ["Adjective", "Nominal"]],
     "Pronoun": [["PRP"], ["PRP$"]],
     "Det": [["DT"]],
-    "Preposition": [["IN"]],
-    "Noun": [["NN"], ["NNS"], ["NNP"],["NNPS"]], # different types of nouns
+    "Prep": [["IN"]],
+    "Noun": [["NN"], ["NNS"], ["NNPS"]], # different types of nouns
     "Verb": [["VB"], ["VBD"], ["VBG"], ["VBN"], ["VBP"], ["VBZ"]],
     "Adjective": [["JJ"], ["JJR"], ["JJS"]]
 }
